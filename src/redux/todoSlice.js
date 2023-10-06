9daafe9ed9993f8990a64e1b2a5128ff7d6615d2
Taharka2000 <136374426+Taharka2000@users.noreply.github.com>
2023-10-06 21:40:45 +0000

    Update todoSlice.js

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,12 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 const todoSlice=createSlice({
     name:"todos",
+    //valeur initial du todos
     initialState:[
         {id:1,title:"todo1",completed:false},
         {id:2,title:"todo2",completed:false},
         {id:3,title:"todo3",completed:true},
     ],
     reducers:{
+        //ajouter un todo
         addTodo:(state,action)=>{
             const newTodo={
                 id:Date.now(),
@@ -15,13 +17,16 @@ const todoSlice=createSlice({
             };
             state.push(newTodo)
         },
+        //voir si un todo est fait ou non
         toggleComplete:(state,action)=>{
             const index=state.findIndex((todo)=>todo.id===action.payload.id)
             state[index].completed=action.payload.completed
         },
+        //suppression d'un todo
         deleteTodo:(state,action)=>{
         return     state.filter((todo)=>todo.id !==action.payload.id)
         },
+        //pour la modification d'un todo
         editTask: (state, action) => {
             const index = state.findIndex(item => item.id === action.payload.id);
             const updatedState = [...state];
