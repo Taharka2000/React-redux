8a893f1315940b8a919baa9c9a407e09e32c4557
Taharka2000 <136374426+Taharka2000@users.noreply.github.com>
2023-10-06 21:37:01 +0000

    Update ListTask.js

--- src/components/ListTask.js
+++ src/components/ListTask.js
@@ -1,9 +1,11 @@
 import React from "react";
 import TodoItem from "./Task";
-import { useSelector } from "react-redux";
+import { useSelector } from "react-redux"
 const TodoList=()=>{
+    //utilisation du useselector pour passer les donnes difinis dans le store et pouvoir l'afficher
     const todos=useSelector((state)=>state.todos)
         return(
+            //utilisation du map pour affichage des donnes de la const todos
           <div className="">
             <ul className="w-[auto] mt-5 ml-[40px] text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
               {todos.map((todo)=>( 
